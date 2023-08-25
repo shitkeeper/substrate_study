f1f98587bcc99ad3a0db17b3c987f62fb8b81058
ed <tomwan95@gmail.com>
2023-08-25 23:45:20 +0800

    Add files via upload

---
+++ target/debug/deps/maopao.d
@@ -0,0 +1,5 @@
+F:\vscode\rust_sub\maopao\target\debug\deps\maopao.exe: src\main.rs
+
+F:\vscode\rust_sub\maopao\target\debug\deps\maopao.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/maopao.d
@@ -0,0 +1,1 @@
+F:\vscode\rust_sub\maopao\target\debug\maopao.exe: F:\vscode\rust_sub\maopao\src\main.rs
