8daa50e08a72e01fed65e2bc46abebd339613fb6
ed <tomwan95@gmail.com>
2023-09-08 09:24:34 +0800

    Add files via upload

---
+++ src_04/target/debug/deps/traffli.d
@@ -0,0 +1,5 @@
+F:\vscode\rust_sub\traffli\target\debug\deps\traffli.exe: src\main.rs
+
+F:\vscode\rust_sub\traffli\target\debug\deps\traffli.d: src\main.rs
+
+src\main.rs:
---
+++ src_04/target/debug/traffli.d
@@ -0,0 +1,1 @@
+F:\vscode\rust_sub\traffli\target\debug\traffli.exe: F:\vscode\rust_sub\traffli\src\main.rs
